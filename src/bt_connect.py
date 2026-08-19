428d1336197620847fc37f7049bd8a43cf93ec92
harmen91 <harmen91@gmail.com>
2026-08-19 16:23:47 +0100

    Various fixes for bt_connect.py

--- src/bt_connect.py
+++ src/bt_connect.py
@@ -4,20 +4,20 @@ import subprocess
 import time
 from load_env import env
 
-#bluetooth hardware controller used for speakers (audio output)
+# BLUETOOTH HARDWARE MAC FOR AUDIO OUTPUT (SPEAKER DEVICES)
 CONTROLLER_OUTPUT = env["CONTROLLER_OUTPUT"]
-#bluetooth hardware controller used to pair phones (audio input)
+# BLUETOOTH HARDWARE MAC FOR AUDIO INPUT (PHONE, COMPUTER, AUDIO STREAM, ETC)
+# USEFULL FOR HAVING TWO SEPERATE BLUETOOTH CONTROLERS HANDLE INCOMING AND OUTGOING 
+# BLUETOOTH STREAMS FOR IMPROVED PERFORMANCE AND REDUCED LATENCY
 CONTROLLER_INPUT = env["CONTROLLER_INPUT"]
 
-SPEAKERS = [
-    env["DIY_SPEAKER_1"],
-    env["DIY_SPEAKER_2"],
-    env["BOSE_SOUNDLINK"],
-    # env["BOSE_SOUNDLINK_HOME"],
-]
+# STORED MAC ADDRESSES OF OUTPUT DEVICES IN ENV
+OUTPUT_DEVICES = [v for k, v in env.items() if k.startswith("OUTPUT_DEVICE")] 
 
-PHONE = env["PHONE_IPHONE"]
- 
+# STORED MAC ADDRESSES OF INPUT DEVICES IN ENV
+INPUT_DEVICES = [v for k, v in env.items() if k.startswith("INPUT_DEVICE")] 
+
+
 # FUNCTION TO INTERACT WITH bluetoothctl
 def bt(script: str) -> str:
     proc = subprocess.run(
@@ -25,6 +25,39 @@ def bt(script: str) -> str:
     )
     return proc.stdout + proc.stderr
 
+def setup_controller(controller: str):
+    print(f"== Setting up bluetooth controller {controller} ==")
+    out = bt(f"select {controller}\npower on\nagent on\ndefault-agent\npairable on")
+    # print(out)
+    time.sleep(1)
+
+# RUNNING SCAN PROCESS IN THE BACKGROUND FOR 60 SECONDS, PROCESS NEEDS TO STAY OPEN FOR SCANNING TO BE ALIVE
+scan_process = None
+def bt_scan_on():
+    global scan_process
+    print("Starting background scan...")
+    scan_process = subprocess.Popen(
+        ["bluetoothctl", "--timeout", "60", "scan", "on"],
+        stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, text=True
+    )
+    time.sleep(1)
+
+# STOPS LAST INSTANCE OF BT_SCAN_ON // DOES NOT ALWAYS 100% WORK
+def bt_scan_stop():
+    global scan_process
+    if scan_process and scan_process.poll() is None:  # still running
+        scan_process.terminate()
+        scan_process.wait()
+        print("Scan stopped.")
+    scan_process = None
+    time.sleep(1)
+
+# KILLS ALL LINGERING BACKGROUND PROCESSES OF BT_SCAN_ON()
+def bt_scan_stop_all():
+    subprocess.run(["pkill", "-f", "bluetoothctl --timeout"])
+    print("Terminated any lingering bluetoothctl scan processes")
+
+# RESTARTS THE BLUETOOTH HARDWARE BY POWERING IT OFF AND ON
 def bt_off_on():
     print("Powering off bluetooth controller")
     bt("power off")
@@ -32,23 +65,34 @@ def bt_off_on():
     print("Powering on bluetooth controller")
     bt("power on")
     time.sleep(1)
+
+# UNUSED, PAIRABLE IS SET IN setup_controller()
+def bt_pairable_on():
+    print("Toggling bluetooth pairable mode on")
+    bt("pairable on")
+    time.sleep(1)
+
+# UNUSED, MAYBE USEFULL FOR BLUETOOTH-IN FUNCTIONALITY LATER
+def bt_discoverable_on():
+    print("Toggling bluetooth discoverable mode on")
+    bt("discoverable on")
+    time.sleep(1)
  
-def setup_controller(controller: str):
-    print(f"== Setting up controller {controller} ==")
-    out = bt(f"select {controller}\npower on\nagent on\ndefault-agent\n")
-    # print(out)
- 
-# REMOVE ALL DEVICES
+# REMOVE ALL TRUSTED, PAIRED and CONNECTED BLUETOOTH DEVICES
 def remove_devices():
-    for mac in SPEAKERS:
+    for mac in OUTPUT_DEVICES:
         bt((f"remove {mac}\n"))
 
 ########################
 
-## Recursively calls itself untill everything is trusted and paired
+## RECURSIVELY CALLS ITSELF UNTILL THE DEVICE IS TRUSTED AND PAIRED, INFINITE LOOP!!
+## WILL NEVER STOP IF THE BLUETOOTH HARDWARE IS OFF OR OUT OF REACH
+## NEEDS IMPROVEMENT ERROR HANDLING, MAX RECURSION DEPTH ETC
+## ALSO TURNS BLUETOOTH POWER AND BLUETOOTH SCAN ON AND OFF DURING EVERY ATTEMPT
 def trust_and_pair_device(mac):
     max_attempts = 10
-    delay = 1
+    delay = 2
+    bt_scan_on()
 
     for attempt in range(max_attempts):
         print(f"{mac} Attempt {attempt + 1}/{max_attempts}")    
@@ -67,37 +111,38 @@ def trust_and_pair_device(mac):
             time.sleep(delay)
             continue
         
-
         return True
     
     print(f"Retrying for {mac}")
+    bt_scan_stop()
     bt_off_on()
     return trust_and_pair_device(mac)
 
 
-# CALL THIS TO PAIR ALL DEVICES
+# CALL THIS TO PAIR ALL OUTPUT_DEVICES FROM ENV
 def pair_all_devices():
     remove_devices()
     time.sleep(1)
-    bt_off_on()
 
-    for mac in SPEAKERS:
+    for mac in OUTPUT_DEVICES:
         trust_and_pair_device(mac)
     
     print("Sucessfully trusted & paired all devices")
 
-def check_if_paired():
-    for mac in SPEAKERS:
+# CHECK IF ALL OUTPUT_DEVICES IN ENV ARE ACTUALLY PAIRED IN BLUETOOTHCTL
+def check_if_paired(verbose = True):
+    for mac in OUTPUT_DEVICES:
         paired = mac in bt("devices Paired")
         if paired:
-            print(f"{mac} paired")
+            if verbose:
+                print(f"{mac} paired")
         else:
-            print(f"{mac} unpaired")
+            if verbose:
+                print(f"{mac} unpaired")
             return False
     return True
 
-##################################
-
+# CONNECT BLUETOOTH DEVICE, NO RECURSION HERE WE ARE ALREADY TRUSTED AND PAIRED, NO SCANNING NECESSARY EITHER
 def bt_connect_device(mac):
     
     max_attempts = 20
@@ -116,22 +161,29 @@ def bt_connect_device(mac):
     print(f"{mac} Failed to connect")
     return False
 
-# CALL THIS TO CONNECT ALL DEVICES
+# CALL THIS TO CONNECT ALL OUTPUT_DEVICES FROM ENV
 def connect_all_devices():
 
-    for mac in SPEAKERS:
+    for mac in OUTPUT_DEVICES:
         bt_connect_device(mac)
         time.sleep(2)
+    
+    print("Succesfully connected all devices")
 
-def check_if_connected() -> (bool, list):
+# CHECK IF ALL OUTPUT_DEVICES IN ENV ARE ACTUALLY CONNECTED IN BLUETOOTHTCL
+# RETURN A BOOL AND LIST
+# LIST USEFULL FOR USE IN AUDIO_SINKS.PY TO CROSS REFERENCE CONNECTED BLUETOOTH WITH PIPEWIRE
+
+def check_if_connected(verbose = True) -> (bool, list):
     list_connected = []
-    for mac in SPEAKERS:
+    for mac in OUTPUT_DEVICES:
         connected = mac in bt("devices Connected")
         if connected:
-            print(f"{mac} connected")
+            if verbose:
+                print(f"{mac} connected")
             list_connected.append(mac)
         else:
-            print(f"{mac} disconnected")
             return False, list_connected
             
+
     return True, list_connected
--- src/main.py
+++ src/main.py
@@ -1,25 +1,27 @@
 import time
 from bt_connect import CONTROLLER_OUTPUT
-from bt_connect import setup_controller, pair_all_devices, connect_all_devices, check_if_connected, check_if_paired, remove_devices
+from bt_connect import setup_controller, pair_all_devices, connect_all_devices, check_if_connected, check_if_paired, remove_devices, bt_scan_stop_all
 
 def main():
-
-    # # initial setup
-    setup_controller(CONTROLLER_OUTPUT)
-    time.sleep(2)
-
 
     # remove_devices() # TEST CODE TO REMOVE DEVICES, not necessary here in final, it also lives in pair_all_device()
     # time.sleep(2)
 
-    # check if all speakers are already trusted & paired
-    if not check_if_paired():
+
+    # # initial setup
+    setup_controller(CONTROLLER_OUTPUT)
+
+    # check if speakers are trusted & paired
+    if not check_if_paired(verbose=False):
         pair_all_devices()
 
-    # check if all speakers are already connected
-    if not check_if_connected()[0]:
+    # check if speakers are connected
+    if not check_if_connected(verbose=False)[0]:
         connect_all_devices()
-    elif check_if_connected()[0]:
-        print("Succesfully connected all devices")
+        bt_scan_stop_all()
+    
+    if check_if_connected()[0]:
+        print("All devices are connected")
+    
 
 main()
