10aa1cbc2eb9ad6eefcf06da19c8f509e4572945
harmen91 <harmen91@gmail.com>
2026-09-01 19:12:11 +0100

    implementing scan_select feature

--- app/scan_select.py
+++ app/scan_select.py
@@ -7,7 +7,7 @@ def scan_to_list():
 
 
     pattern = r"\b(?:[0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}\b"
-    lines_with_macs = []
+    new_devices_list = []
     local_scan_lines = []
 
 
@@ -28,16 +28,16 @@ def scan_to_list():
             line = scan_queue.get(timeout=1)
             if re.search(pattern, line): 
                 found_mac = re.findall(pattern, line)
-                if found_mac not in lines_with_macs:
+                if found_mac not in    new_devices_list:
                     if "[NEW] Device" in line: #only append "[NEW] Device AA:BB:CC:DD"
                         device_mac = line.split(maxsplit=3)[2]
                         device_name = line.split(maxsplit=3)[3]
-                        lines_with_macs.append((
+                        new_devices_list.append((
                             device_mac,
                             device_name
                             ))
             time.sleep(1)
-            print(f"TESSTTT>>>>>>{lines_with_macs}")
+            print(f"TESSTTT>>>>>>      new_devices_list}")
         except queue.Empty:
             print(f"Waiting for more devices to appear..") #waiting for new entry in queue to appear
         
